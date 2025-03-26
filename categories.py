b983f5dd1ae62b17e0e03496c6a7b1c6d1e0bfc4
Arthur <arthur@freye.in>
2025-03-26 00:25:42 +0000

    Save adjusted category mapping
    Delete unused intermediate CSV data files

--- categories.py
+++ categories.py
@@ -11,3 +11,11 @@ def load():
 def filter(df, category):
     columns = load().get(category, [])
     return df[columns]
+
+
+def drop(json_data, columns):
+    for key in json_data.keys():
+        for column in columns:
+            if column in json_data[key]:
+                json_data[key].remove(column)
+    return json_data
